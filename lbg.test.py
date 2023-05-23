079aef61e354287a2e170b077ebf4ac9b4421cd1
Tim Christopher <66315346+timchristopher@users.noreply.github.com>
2023-05-23 13:56:08 +0100

    Update lbg.test.py - hard coded port to 5000

--- lbg.test.py
+++ lbg.test.py
@@ -11,7 +11,7 @@ from flask_api import status
 import requests
 from sys import argv 
 
-PORT = int(argv[1])
+PORT = 5000 #int(argv[1])
 BASE_URL = f"http://localhost:{PORT}"
 
 class MyLbgApiTestCase(unittest.TestCase):
