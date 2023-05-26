f3d5c53a992981d4c6833579f6fca434d4a32512
Tim Christopher <66315346+timchristopher@users.noreply.github.com>
2023-05-26 08:58:25 +0100

    Update lbg.py - Use LBG_PYTHON_PORT to define default port

--- lbg.py
+++ lbg.py
@@ -16,6 +16,9 @@ from flask_api import status
 from models import db, ItemModel
 from sqlalchemy import exc
 
+# Provide access to environment variables
+from os import getenv
+
 # JavaScript/ES6 text/plain MIME Content type fix (avoids registry hack!)
 import mimetypes
 mimetypes.add_type('text/javascript', '.js')
@@ -208,7 +211,7 @@ def delete_one(_id):
 if __name__ == '__main__':
     # set up the app with listening socket for http requests and appropriate hostname
     parser = argparse.ArgumentParser()
-    parser.add_argument("--PORT", default="8080")
+    parser.add_argument("--PORT", default=getenv('LBG_PYTHON_PORT'))
     args = parser.parse_args()
 
     PORT = args.PORT
