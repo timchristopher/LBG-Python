6c664c02b2e54dfeef0d241619316870cabd6387
Tim Christopher <66315346+timchristopher@users.noreply.github.com>
2023-05-26 18:12:29 +0100

    Update lbg.py

--- lbg.py
+++ lbg.py
@@ -211,7 +211,7 @@ def delete_one(_id):
 if __name__ == '__main__':
     # set up the app with listening socket for http requests and appropriate hostname
     parser = argparse.ArgumentParser()
-    parser.add_argument("--PYTHON_HOST", default="127.0.0.1")
+    parser.add_argument("--PYTHON_HOST", default="0.0.0.0")
     parser.add_argument("--PORT", default=getenv('PORT'))
 
     args = parser.parse_args()
